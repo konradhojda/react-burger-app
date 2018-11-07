a4908ff2f2f8bb97071278eaf213205609a76b46
Konrad <konrad.hojda@gmail.com>
2018-11-07 23:13:25 +0100

    little modifications about two-way binding and showing element depends of value of variable

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ class App extends Component {
                 name: 'Patricia',
                 age: 22
             }
-        ]
+        ],
+        showPersons: false
     };
 
     changeName = () => {
@@ -40,15 +41,55 @@ class App extends Component {
         })
     };
 
+    changedValue = (e) => {
+        this.setState({
+            persons: [
+                {
+                    name: e.target.value,
+                    age: 20
+                },
+                {
+                    name: 'Antony',
+                    age: 25
+                },
+                {
+                    name: 'Patricia',
+                    age: 22
+                }
+            ]
+        })
+    };
+
+    togglePersons = () => {
+        this.setState({
+            showPersons: true
+        });
+    };
+
     render() {
+
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person changed={this.changedValue}
+                            name={this.state.persons[0].name}
+                            age={this.state.persons[0].age}/>
+                    <Person name={this.state.persons[1].name}
+                            age={this.state.persons[1].age}/>
+                    <Person name={this.state.persons[2].name}
+                            age={this.state.persons[2].age}/>
+                </div>
+            );
+        }
+
         return (
             <div className="App">
                 <h1>Simple React Header</h1>
                 <p>It's working!</p>
-                <button onClick={this.changeName}>Click to switch the name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>{1+2}</Person>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                <button onClick={this.togglePersons}>Click to switch the name</button>
+                {persons}
             </div>
         );
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,7 @@ const person = (props) => {
     return (
         <div>
             <p>I'm {props.name} and I have {props.age} years old!</p>
+            <input type="text" onChange={props.changed}/>
             <p>{props.children}</p>
         </div>
     )
