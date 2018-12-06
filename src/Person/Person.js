8f403e99b386ae2dc08d3dd0e5bb972987f03d4f
Konrad <konrad.hojda@gmail.com>
2018-12-06 23:14:23 +0100

    change changedValue of person method

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 
 const person = (props) => {
     return (
-        <div>
-            <p>I'm {props.name} and I have {props.age} years old!</p>
-            <input type="text" onChange={props.changed}/>
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I have {props.age} years old!</p>
+            <input type="text" onChange={props.changed} placeholder={props.name}/>
             <p>{props.children}</p>
         </div>
     )
