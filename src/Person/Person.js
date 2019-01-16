92c010568a0f9da3b8bddf1049b293f02001d566
konrad.hojda <konrad.hojda@codete.com>
2019-01-16 14:08:19 +0100

    easy change line for try

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 const person = (props) => {
     return (
         <div>
             <p>I'm {props.name} and I have {props.age} years old!</p>
             <input type="text" onChange={props.changed}/>
-            <p>{props.children}</p>
+            <p>{props.children}{2+2}</p>
         </div>
     )
 };
