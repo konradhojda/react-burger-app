1c408d72943040271d47f5497fdaf300f6980c3a
konrad.hojda <konrad.hojda@codete.com>
2019-01-17 15:29:44 +0100

    changes with structure of project, did project eject and configure webpack

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -75,19 +75,19 @@ class App extends Component {
             style.backgroundColor = 'red';
         }
 
-        let classes = [];
+        const assignedClass = [];
         if (this.state.persons.length <= 2) {
-            classes.push('red');
+            assignedClass.push(classes.red);
         }
         if (this.state.persons.length <= 1) {
-            classes.push('bold');
+            assignedClass.push(classes.bold);
         }
 
 
         return (
-                <div className="App">
+                <div className={classes.App}>
                     <h1>Simple React Header</h1>
-                    <p className={classes.join(' ')}>It's working!</p>
+                    <p className={assignedClass.join(' ')}>It's working!</p>
                     <button style={style} onClick={this.togglePersons}>Click to switch the
                         name
                     </button>
