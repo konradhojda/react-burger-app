e0bb5626ae66edb6f2da07a1e44c3ecde807b02e
konrad.hojda <konrad.hojda@codete.com>
2019-01-18 15:22:18 +0100

    changes about structure in project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 
 
 class App extends Component {
@@ -57,10 +57,10 @@ class App extends Component {
                 <div>
                     {this.state.persons.map((person, index) => {
                         return <Person name={person.name}
-                                       age={person.age}
-                                       click={this.deletePerson.bind(index)}
-                                       key={person.id}
-                                       changed={(e) => this.changedValue(e, person.id)}/>
+                                    age={person.age}
+                                    click={this.deletePerson.bind(index)}
+                                    key={person.id}
+                                    changed={(e) => this.changedValue(e, person.id)}/>
                     })}
                 </div>
             );
