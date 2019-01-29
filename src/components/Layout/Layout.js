09412ab8efa2efb3e39570056fdfba87d49cf6eb
konradhojda <konrad.hojda@codete.com>
2019-01-29 16:51:54 +0100

    - added reusable nav items - created responsive sidedreawer - added rwd adjustments

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawerm from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <Toolbar/>
+        <SideDrawerm/>
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
